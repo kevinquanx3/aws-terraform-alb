99ac9389a2704d067ac8159b63823fa059d67fbf
Carlo Taguinod <carlo.taguinod@rackspace.com>
2019-07-15 15:22:28 +0800

    updated terraform 0.12 example

--- examples/terraform012_example/outputs.tf
+++ examples/terraform012_example/outputs.tf
@@ -8,14 +8,6 @@ output "summary" {
 
 ## Outputs - 100compute layer
 
-|  | Key Pairs |
-|---|---|
-| key_pair_internal | ${aws_key_pair.key_pair_internal.key_name} |
-| key_pair_external | ${aws_key_pair.key_pair_external.key_name} |
-| key_pair_external_bastion | ${aws_key_pair.key_pair_external_bastion.key_name} |
-
-
-
 | ALB Name | Endpoint |
 |---|---|
 | ALB | ${module.alb.alb_dns_name} |
