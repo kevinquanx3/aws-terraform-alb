64d8df4beb1da36eb221bd5a0026de87ffcace67
kevinquanx3 <kevin.quan@sage.com>
2020-01-22 08:28:44 -0800

    rack to ours

--- main.tf
+++ main.tf
@@ -189,7 +189,7 @@ data "null_data_source" "alarm_dimensions" {
 }
 
 module "unhealthy_host_count_alarm" {
-  source = "git@github.com:rackspace-infrastructure-automation/aws-terraform-cloudwatch_alarm//?ref=v0.0.1"
+  source = "git@github.com:kevinquanx3/aws-terraform-cloudwatch_alarm//?ref=v0.0.1"
 
   alarm_count              = "${var.target_groups_count > 0 ? var.target_groups_count:0}"
   alarm_description        = "Unhealthy Host count is greater than or equal to threshold, creating ticket."
